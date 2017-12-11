9837c0f5bc4037e50eb00f00ba0d59cfca0db3c8
Kuba Mikołajczyk <qba600@gmail.com>
2017-12-11 23:38:35 +0100

    Pass book address test

--- address_book.py
+++ address_book.py
@@ -1,10 +1,12 @@
 from address import Address
 from work_address import WorkAddress
 
+
 class AddressBook:
     
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.addresses = []
 
     def add_address(self, address):
        pass
