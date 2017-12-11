add2ed4ac1664450201a3e4eef73809a081fdfd8
Kuba Mikołajczyk <qba600@gmail.com>
2017-12-11 23:34:06 +0100

    Add workAddress init with inheritance / passing workAddress person test

--- work_address.py
+++ work_address.py
@@ -3,8 +3,9 @@ from address import Address
 
 class WorkAddress(Address):
 
-    def __init__(self):
-        pass
+    def __init__(self, person, city, street, house_no, company):
+        super().__init__(person, city, street, house_no)
+        self.company = company
 
     def get_full_address(self):
         pass
