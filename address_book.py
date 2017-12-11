edc672845a9deaeb574eb58407c80369af2440ea
Kuba Mikołajczyk <qba600@gmail.com>
2017-12-12 00:46:20 +0100

    Pass case insensitivity test

--- address_book.py
+++ address_book.py
@@ -18,10 +18,10 @@ class AddressBook:
         found_list = []
 
         for adress in self.addresses:
-            if search_phrase in adress.person \
-               or search_phrase in adress.city \
-               or search_phrase in adress.street \
-               or search_phrase in adress.house_no:
+            if search_phrase.lower() in adress.person.lower() \
+               or search_phrase.lower() in adress.city.lower() \
+               or search_phrase.lower() in adress.street.lower() \
+               or search_phrase.lower() in adress.house_no.lower():
                 found_list.append(adress)
         
         return found_list
