53516b659603159134fd7799a756580dd8b80dc7
Kuba Mikołajczyk <qba600@gmail.com>
2017-12-11 23:42:01 +0100

    Pass get full address test

--- address.py
+++ address.py
@@ -7,7 +7,7 @@ class Address:
         self.house_no = house_no
 
     def get_full_address(self):
-        pass
+        return "{}, {}, {} {}".format(self.person, self.city, self.street, self.house_no)
     
     def __eq__(self, other):
         pass
